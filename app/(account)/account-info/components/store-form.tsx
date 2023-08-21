1101c5aee0eceb6aa810fe597c0087a6fe461f38
reshaditiya <reshaditiya@gmail.com>
2023-08-21 12:53:19 +0700

    account setting form

--- app/(account)/account-info/components/store-form.tsx
+++ app/(account)/account-info/components/store-form.tsx
@@ -1,195 +1,211 @@
-"use client";
+'use client';
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-import { Button } from "@/components/ui/button";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { Button } from '@/components/ui/button';
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import { toast } from "@/components/ui/use-toast";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useEffect } from "react";
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { toast } from '@/components/ui/use-toast';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useRouter } from 'next/navigation';
+import { ToastAction } from '@/components/ui/toast';
 
 const storeFormSchema = z.object({
-  nama_toko: z
-    .string()
-    .min(4, {
-      message: "Nama toko harus lebih dari 4 karakter",
-    })
-    .max(20, {
-      message: "Nama toko tidak boleh lebih dari 20 karakter",
-    }),
-  foto: z.string().optional(),
-  kabupaten: z
-    .string()
-    .min(4, {
-      message: "Kabupaten harus lebih dari 4 karakter",
-    })
-    .max(20, {
-      message: "Kabupaten tidak boleh lebih dari 20 karakter",
-    }),
-  kecamatan: z
-    .string()
-    .min(4, {
-      message: "Kecamatan harus lebih dari 4 karakter",
-    })
-    .max(20, {
-      message: "Kecamatan tidak boleh lebih dari 20 karakter",
-    }),
-  desa: z
-    .string()
-    .min(4, {
-      message: "Desa harus lebih dari 4 karakter",
-    })
-    .max(20, {
-      message: "Desa tidak boleh lebih dari 20 karakter",
-    }),
-  alamat: z
-    .string()
-    .min(4, {
-      message: "Desa harus lebih dari 5 karakter",
-    })
-    .max(20, {
-      message: "Desa tidak boleh lebih dari 20 karakter",
-    }),
+	nama_toko: z
+		.string()
+		.min(4, {
+			message: 'Nama toko harus lebih dari 4 karakter',
+		})
+		.max(20, {
+			message: 'Nama toko tidak boleh lebih dari 20 karakter',
+		}),
+	foto_url: z.string().optional(),
+	kabupaten: z
+		.string()
+		.min(4, {
+			message: 'Kabupaten harus lebih dari 4 karakter',
+		})
+		.max(20, {
+			message: 'Kabupaten tidak boleh lebih dari 20 karakter',
+		}),
+	kecamatan: z
+		.string()
+		.min(4, {
+			message: 'Kecamatan harus lebih dari 4 karakter',
+		})
+		.max(20, {
+			message: 'Kecamatan tidak boleh lebih dari 20 karakter',
+		}),
+	desa: z
+		.string()
+		.min(4, {
+			message: 'Desa harus lebih dari 4 karakter',
+		})
+		.max(20, {
+			message: 'Desa tidak boleh lebih dari 20 karakter',
+		}),
+	alamat: z
+		.string()
+		.min(4, {
+			message: 'Desa harus lebih dari 5 karakter',
+		})
+		.max(20, {
+			message: 'Desa tidak boleh lebih dari 20 karakter',
+		}),
 });
 
 type ProfileFormValues = z.infer<typeof storeFormSchema>;
 
-export default function StoreForm() {
-  const supabase = createClientComponentClient();
+export default function StoreForm({
+	store,
+}: {
+	store: {
+		nama_toko: string | null;
+		kabupaten: string | null;
+		kecamatan: string | null;
+		desa: string | null;
+		alamat: string | null;
+		foto_url: string | null;
+	} | null;
+}) {
+	const router = useRouter();
+	const supabase = createClientComponentClient();
+	const defaultValues: Partial<ProfileFormValues> = {
+		nama_toko: store?.nama_toko ?? '',
+		kabupaten: store?.kabupaten ?? '',
+		kecamatan: store?.kecamatan ?? '',
+		desa: store?.desa ?? '',
+		alamat: store?.alamat ?? '',
+		foto_url: '',
+	};
 
-  const defaultValues: Partial<ProfileFormValues> = {
-    nama_toko: "",
-    kabupaten: "",
-    kecamatan: "",
-    desa: "",
-    alamat: "",
-    foto: "",
-  };
+	const form = useForm<ProfileFormValues>({
+		resolver: zodResolver(storeFormSchema),
+		defaultValues,
+		mode: 'onChange',
+	});
 
-  useEffect(() => {
-    async function getData() {
-      const userData = await supabase.auth.getUser();
+	async function onSubmit(data: ProfileFormValues) {
+		const user = await supabase.auth.getUser();
+		const { error } = await supabase
+			.from('toko')
+			.update({
+				nama_toko: data.nama_toko,
+				kabupaten: data.kabupaten,
+				kecamatan: data.kecamatan,
+				desa: data.desa,
+				alamat: data.alamat,
+				foto_url: data.foto_url,
+			})
+			.eq('id', user.data.user?.id);
 
-      if (userData.data.user) {
-        await supabase
-          .from("toko")
-          .select("nama_toko, kabupaten, kecamatan, desa, alamat, foto")
-          .eq("id", userData.data.user.id);
-      }
-    }
-  }, [supabase]);
+		if (error) {
+			toast({
+				variant: 'destructive',
+				title: 'Data Gagal Disimpan!',
+				description: 'Terjadi kesalahan saat menyimpan data silahkan coba lagi',
+				action: <ToastAction altText='Coba Lagi'>Coba Lagi</ToastAction>,
+			});
+		} else {
+			toast({
+				description: 'Data toko berhasil di update!',
+			});
+			router.refresh();
+		}
+	}
 
-  const form = useForm<ProfileFormValues>({
-    resolver: zodResolver(storeFormSchema),
-    defaultValues,
-    mode: "onChange",
-  });
-
-  function onSubmit(data: ProfileFormValues) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    });
-    form.reset();
-  }
-
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="nama_toko"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Nama Toko</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="foto"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Foto</FormLabel>
-              <FormControl>
-                <Input type="file" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="kabupaten"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Kabupaten</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="kecamatan"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Kecamatan</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="desa"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Desa</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="alamat"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Alamat</FormLabel>
-              <FormControl>
-                <Textarea {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Simpan</Button>
-      </form>
-    </Form>
-  );
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+				<FormField
+					control={form.control}
+					name='nama_toko'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Nama Toko</FormLabel>
+							<FormControl>
+								<Input {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name='foto_url'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Foto</FormLabel>
+							<FormControl>
+								<Input type='file' {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name='kabupaten'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Kabupaten</FormLabel>
+							<FormControl>
+								<Input {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name='kecamatan'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Kecamatan</FormLabel>
+							<FormControl>
+								<Input {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name='desa'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Desa</FormLabel>
+							<FormControl>
+								<Input {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name='alamat'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Alamat</FormLabel>
+							<FormControl>
+								<Textarea {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type='submit'>Simpan</Button>
+			</form>
+		</Form>
+	);
 }
--- app/(account)/account-info/owner/components/owner-form.tsx
+++ app/(account)/account-info/owner/components/owner-form.tsx
@@ -1,117 +1,135 @@
-"use client";
+'use client';
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-
-import { Button } from "@/components/ui/button";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { Button } from '@/components/ui/button';
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import { toast } from "@/components/ui/use-toast";
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useRouter } from 'next/navigation';
+import { toast } from '@/components/ui/use-toast';
+import { ToastAction } from '@/components/ui/toast';
 
 const storeFormSchema = z.object({
-  nama_pemilik: z
-    .string()
-    .min(4, {
-      message: "Nama pemilik harus lebih dari 4 karakter",
-    })
-    .max(20, {
-      message: "Nama pemilik tidak boleh lebih dari 20 karakter",
-    }),
-  email: z.string().min(4, {
-    message: "Email harus lebih dari 4 karakter",
-  }),
-  no_hp: z
-    .string()
-    .min(4, {
-      message: "No hp harus lebih dari 4 karakter",
-    })
-    .max(15, {
-      message: "No hp tidak boleh lebih dari 15 karakter",
-    }),
+	nama_pemilik: z
+		.string()
+		.min(4, {
+			message: 'Nama pemilik harus lebih dari 4 karakter',
+		})
+		.max(20, {
+			message: 'Nama pemilik tidak boleh lebih dari 20 karakter',
+		}),
+	email: z.string().min(4, {
+		message: 'Email harus lebih dari 4 karakter',
+	}),
+	no_hp: z
+		.string()
+		.min(4, {
+			message: 'No hp harus lebih dari 4 karakter',
+		})
+		.max(15, {
+			message: 'No hp tidak boleh lebih dari 15 karakter',
+		}),
 });
 
 type ProfileFormValues = z.infer<typeof storeFormSchema>;
 
-// This can come from your database or API.
-const defaultValues: Partial<ProfileFormValues> = {
-  // bio: 'I own a computer.',
-  // urls: [
-  // 	{ value: 'https://shadcn.com' },
-  // 	{ value: 'http://twitter.com/shadcn' },
-  // ],
-};
+export default function OwnerForm({
+	owner,
+}: {
+	owner: {
+		nama_pemilik: string | null;
+		email: string | null;
+		no_hp: string | null;
+	} | null;
+}) {
+	const router = useRouter();
+	const supabase = createClientComponentClient<Database>();
+	const defaultValues: Partial<ProfileFormValues> = {
+		nama_pemilik: owner?.nama_pemilik ?? '',
+		email: owner?.email ?? '',
+		no_hp: owner?.no_hp ?? '',
+	};
+	const form = useForm<ProfileFormValues>({
+		resolver: zodResolver(storeFormSchema),
+		defaultValues,
+		mode: 'onChange',
+	});
 
-export default function OwnerForm() {
-  const form = useForm<ProfileFormValues>({
-    resolver: zodResolver(storeFormSchema),
-    defaultValues,
-    mode: "onChange",
-  });
+	async function onSubmit(data: ProfileFormValues) {
+		const user = await supabase.auth.getUser();
+		const { error } = await supabase
+			.from('toko')
+			.update({ nama_pemilik: data.nama_pemilik, no_hp: data.no_hp })
+			.eq('id', user.data.user?.id);
 
-  function onSubmit(data: ProfileFormValues) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    });
-  }
+		if (error) {
+			toast({
+				variant: 'destructive',
+				title: 'Data Gagal Disimpan!',
+				description: 'Terjadi kesalahan saat menyimpan data silahkan coba lagi',
+				action: <ToastAction altText='Coba Lagi'>Coba Lagi</ToastAction>,
+			});
+		} else {
+			toast({
+				description: 'Data pemilik toko berhasil di update!',
+			});
+			router.refresh();
+		}
+	}
 
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="nama_pemilik"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Nama Pemilik</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input type="email" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="no_hp"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>No Hp</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Simpan</Button>
-      </form>
-    </Form>
-  );
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+				<FormField
+					control={form.control}
+					name='nama_pemilik'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Nama Pemilik</FormLabel>
+							<FormControl>
+								<Input {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name='email'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Email</FormLabel>
+							<FormControl>
+								<Input type='email' {...field} disabled />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name='no_hp'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>No Hp</FormLabel>
+							<FormControl>
+								<Input {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type='submit'>Simpan</Button>
+			</form>
+		</Form>
+	);
 }
