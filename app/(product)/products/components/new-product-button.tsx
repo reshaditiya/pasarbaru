0fefade15da58c88da5bf2e2bb9b1e579d67dc7d
reshaditiya <reshaditiya@gmail.com>
2023-08-23 09:14:22 +0700

    cleanup

--- app/(product)/products/components/new-product-button.tsx
+++ app/(product)/products/components/new-product-button.tsx
@@ -10,7 +10,6 @@ import {
 	DialogTitle,
 	DialogTrigger,
 } from '@/components/ui/dialog';
-import { Close } from '@radix-ui/react-dialog';
 import { toast } from '@/components/ui/use-toast';
 import {
 	Form,
