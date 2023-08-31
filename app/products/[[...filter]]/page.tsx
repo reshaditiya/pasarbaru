d47b81497161642bb67e97e3beefa8a53a2867bf
reshaditiya <reshaditiya@gmail.com>
2023-08-31 14:54:34 +0700

    show 3 card column

--- app/products/[[...filter]]/page.tsx
+++ app/products/[[...filter]]/page.tsx
@@ -3,32 +3,32 @@ import ProductCard from '../components/product-card';
 import { cookies } from 'next/headers';
 
 export default async function Page({
-	params,
+  params,
 }: {
-	params: { filter: string[] };
+  params: { filter: string[] };
 }) {
-	const [filterJenis, filterKeyword] = params.filter ?? [];
-	const supabase = createServerComponentClient<Database>({ cookies });
-	const user = await supabase.auth.getUser();
-	const queryProducts = supabase
-		.from('produk')
-		.select(
-			'id, nama, jenis, harga_beli, harga_jual, foto1, foto2, foto3, satuan'
-		)
-		.eq('id_toko', user.data.user?.id)
-		.order('created_at', { ascending: true });
+  const [filterJenis, filterKeyword] = params.filter ?? [];
+  const supabase = createServerComponentClient<Database>({ cookies });
+  const user = await supabase.auth.getUser();
+  const queryProducts = supabase
+    .from('produk')
+    .select(
+      'id, id_toko, nama, jenis, harga_beli, harga_jual, foto1, foto2, foto3, satuan',
+    )
+    .eq('id_toko', user.data.user?.id)
+    .order('created_at', { ascending: true });
 
-	if (filterJenis && filterJenis !== 'all')
-		queryProducts.eq('jenis', decodeURI(filterJenis));
-	if (filterKeyword) queryProducts.ilike('nama', `%${filterKeyword}%`);
+  if (filterJenis && filterJenis !== 'all')
+    queryProducts.eq('jenis', decodeURI(filterJenis));
+  if (filterKeyword) queryProducts.ilike('nama', `%${filterKeyword}%`);
 
-	const products = await queryProducts;
+  const products = await queryProducts;
 
-	return (
-		<section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4  gap-4 w-full'>
-			{products.data?.map((product) => (
-				<ProductCard product={product} key={product.id} />
-			))}
-		</section>
-	);
+  return (
+    <section className="grid w-full grid-cols-1 gap-4  sm:grid-cols-2 md:grid-cols-3">
+      {products.data?.map((product) => (
+        <ProductCard product={product} key={product.id} />
+      ))}
+    </section>
+  );
 }
