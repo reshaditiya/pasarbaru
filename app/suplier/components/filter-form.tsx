ab10a45876f186a61f479f870404a060bacbfe77
reshaditiya <reshaditiya@gmail.com>
2023-09-01 13:22:33 +0700

    responsive layout

--- app/suplier/components/filter-form.tsx
+++ app/suplier/components/filter-form.tsx
@@ -1,90 +1,121 @@
 'use client';
 
 import {
-	Card,
-	CardContent,
-	CardFooter,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
-import { ArrowsClockwise } from '@/components/icon/phospor';
+import { ArrowsClockwise, Sliders } from '@/components/icon/phospor';
 import { Separator } from '@/components/ui/separator';
 import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from '@/components/ui/form';
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Input } from '@/components/ui/input';
 import { useRouter } from 'next/navigation';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
 
 const FilterSchema = z.object({
-	keyword: z.string().optional(),
+  keyword: z.string().optional(),
 });
 
 const defaultValues: Partial<FilterValues> = {
-	keyword: '',
+  keyword: '',
 };
 
 type FilterValues = z.infer<typeof FilterSchema>;
 
 export function FilterForm() {
-	const router = useRouter();
-	const form = useForm<FilterValues>({
-		resolver: zodResolver(FilterSchema),
-		defaultValues,
-		mode: 'onChange',
-	});
+  return (
+    <>
+      <FilterFormPopover className="lg:hidden" />
+      <FilterFormCard className="hidden lg:block" />
+    </>
+  );
+}
 
-	function onSubmit(data: FilterValues) {
-		router.push(`/suplier/${data.keyword ?? ''}`);
-	}
+export function FilterFormPopover(
+  props: React.ButtonHTMLAttributes<HTMLButtonElement>,
+) {
+  return (
+    <Popover>
+      <PopoverTrigger {...props} asChild>
+        <Button variant="outline" size="icon">
+          <Sliders className="h-5 w-5" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="p-0" align="end">
+        <FilterFormCard className="border-none p-0 shadow-none" />
+      </PopoverContent>
+    </Popover>
+  );
+}
 
-	function handleReset(e: any) {
-		e.preventDefault();
-		form.reset();
-		router.push('/suplier');
-	}
+export function FilterFormCard(props: React.HTMLAttributes<HTMLDivElement>) {
+  const router = useRouter();
+  const form = useForm<FilterValues>({
+    resolver: zodResolver(FilterSchema),
+    defaultValues,
+    mode: 'onChange',
+  });
 
-	return (
-		<Card>
-			<CardHeader>
-				<CardTitle>Filter Data</CardTitle>
-				<Separator />
-			</CardHeader>
-			<Form {...form}>
-				<form onSubmit={form.handleSubmit(onSubmit)}>
-					<CardContent className='flex flex-col gap-4'>
-						<FormField
-							control={form.control}
-							name='keyword'
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Cari</FormLabel>
-									<FormControl>
-										<Input {...field} />
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-					</CardContent>
-					<CardFooter className='flex gap-2 justify-end'>
-						<Button variant='ghost' size='icon' onClick={handleReset}>
-							<ArrowsClockwise className='h-4 w-4' />
-						</Button>
-						<Button variant='secondary' type='submit'>
-							Terapkan
-						</Button>
-					</CardFooter>
-				</form>
-			</Form>
-		</Card>
-	);
+  function onSubmit(data: FilterValues) {
+    router.push(`/suplier/${data.keyword ?? ''}`);
+  }
+
+  function handleReset(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    form.reset();
+    router.push('/suplier');
+  }
+
+  return (
+    <Card {...props}>
+      <CardHeader>
+        <CardTitle>Filter Data</CardTitle>
+        <Separator />
+      </CardHeader>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <CardContent className="flex flex-col gap-4">
+            <FormField
+              control={form.control}
+              name="keyword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Cari</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+          <CardFooter className="flex justify-end gap-2">
+            <Button variant="ghost" size="icon" onClick={(e) => handleReset(e)}>
+              <ArrowsClockwise className="h-4 w-4" />
+            </Button>
+            <Button variant="secondary" type="submit">
+              Terapkan
+            </Button>
+          </CardFooter>
+        </form>
+      </Form>
+    </Card>
+  );
 }
--- app/suplier/components/sidebar-nav.tsx
+++ app/suplier/components/sidebar-nav.tsx
@@ -2,10 +2,10 @@ import { FilterForm } from './filter-form';
 import { SuplierFormButton } from './suplier-form-button';
 
 export default function SidebarNav() {
-	return (
-		<nav className='flex gap-4 lg:flex-col lg:space-x-0 lg:space-y-1'>
-			<SuplierFormButton type='new' />
-			<FilterForm />
-		</nav>
-	);
+  return (
+    <nav className="flex justify-between gap-4 lg:flex-col lg:space-x-0 lg:space-y-1">
+      <SuplierFormButton type="new" />
+      <FilterForm />
+    </nav>
+  );
 }
