4ab0ccf02a91f44d1553ab7c0cddc22b598c8414
reshaditiya <reshaditiya@gmail.com>
2023-09-01 11:32:19 +0700

    padding layout responsive

--- app/products/layout.tsx
+++ app/products/layout.tsx
@@ -3,22 +3,22 @@ import SidebarNav from './components/sidebar-nav';
 import { Separator } from '@/components/ui/separator';
 
 export default function ProductsLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return withAuth(
-		<div className='space-y-4 p-10 pb-16'>
-			<div>
-				<h2 className='text-xl font-bold tracking-tight'>Produk</h2>
-			</div>
-			<Separator className='my-6' />
-			<div className='flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0'>
-				<aside className='lg:w-1/4'>
-					<SidebarNav />
-				</aside>
-				{children}
-			</div>
-		</div>
-	);
+  return withAuth(
+    <div className="space-y-4 p-5 pb-16 md:p-10">
+      <div>
+        <h2 className="text-xl font-bold tracking-tight">Produk</h2>
+      </div>
+      <Separator className="my-6" />
+      <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+        <aside className="lg:w-1/4">
+          <SidebarNav />
+        </aside>
+        {children}
+      </div>
+    </div>,
+  );
 }
