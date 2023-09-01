3131adb368c1cc00d4dd59a727d969a5dba173a6
reshaditiya <reshaditiya@gmail.com>
2023-09-01 13:57:42 +0700

    responsive layout

--- app/(account)/account-info/layout.tsx
+++ app/(account)/account-info/layout.tsx
@@ -25,14 +25,14 @@ interface SettingsLayoutProps {
 
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return withAuth(
-    <div className="space-y-6 p-10 pb-16">
+    <div className="space-y-6 p-5 pb-16 md:p-10">
       <div className="space-y-0.5">
         <h2 className="text-2xl font-bold tracking-tight">Pengaturan</h2>
         <p className="text-muted-foreground">Kelola data akun dan toko anda.</p>
       </div>
       <Separator className="my-6" />
       <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-        <aside className="-mx-4 lg:w-1/5">
+        <aside className="lg:w-1/4">
           <SidebarNav items={sidebarNavItems} />
         </aside>
         <div className="flex-1 lg:max-w-2xl">{children}</div>
