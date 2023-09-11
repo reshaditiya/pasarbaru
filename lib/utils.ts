7df4c42bcae3de5139948f2539d477af0f8c8339
reshaditiya <reshaditiya@gmail.com>
2023-09-11 22:02:15 +0700

    edit minor naming

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatTothousand(number: number) {
+export function formatToThousand(number: number) {
   const formatter = new Intl.NumberFormat('id-ID');
 
   return formatter.format(number);
