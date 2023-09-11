28fdeb8bf0b16df847c7f1f1d321ec18495b41b2
reshaditiya <reshaditiya@gmail.com>
2023-09-11 22:01:14 +0700

    add formatTothousand util

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatTothousand(number: number) {
+  const formatter = new Intl.NumberFormat('id-ID');
+
+  return formatter.format(number);
+}
