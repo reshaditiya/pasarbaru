e71e014c40a2fe98641575e3e20f019dd610d6c5
reshaditiya <reshaditiya@gmail.com>
2023-09-12 09:26:17 +0700

    add max width to the website

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} mx-auto max-w-7xl`}>
         {session && <Navbar />}
         {children}
         <Toaster />
